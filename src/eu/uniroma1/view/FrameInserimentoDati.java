e192ed80604b64d9c212483083efc8b67331acaf
Tommaso Bruni <tommasobruni071@gmail.com>
2023-05-25 11:01:00 +0200

    added items

--- src/eu/uniroma1/view/FrameInserimentoDati.java
+++ src/eu/uniroma1/view/FrameInserimentoDati.java
@@ -22,20 +22,22 @@ public class FrameInserimentoDati extends JInternalFrame
 	{
 		super("Inserimento dati",
 		          false, //resizable
-		          true, //closable
+		          false, //closable
 		          true, //maximizable
 		          true);
 		labelNomeUtente = new JLabel("Nome utente:");
 		textFieldNomeUtente = new JTextField(15);
 		labelNickname = new JLabel("Nickname:");
+		textFieldNickname = new JTextField(15);
 		pannelloContenitore = new JPanel();
-		
-		//pack();
+
 		setSize(200, 200);
 		setLocation(100, 100);
 		
 		pannelloContenitore.add(labelNomeUtente);
 		pannelloContenitore.add(textFieldNomeUtente);
+		pannelloContenitore.add(labelNickname);
+		pannelloContenitore.add(textFieldNickname);
 		add(pannelloContenitore);
 	}
 }
