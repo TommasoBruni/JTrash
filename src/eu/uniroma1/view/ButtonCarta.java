da96fb64e11b4cbfe8ea56d52bff4f645f57dcf4
Tommaso Bruni <tommasobruni071@gmail.com>
2023-05-31 17:22:15 +0200

    fixed position of drawing card

--- src/eu/uniroma1/view/ButtonCarta.java
+++ src/eu/uniroma1/view/ButtonCarta.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import java.awt.geom.AffineTransform;
+import java.awt.Graphics2D;
 
 public class ButtonCarta extends JButton
 {
@@ -16,6 +18,7 @@ public class ButtonCarta extends JButton
 	private static final String fileCartaGiocoVerticale = "carta_da_gioco_verticale.jpg";
 	private static final String fileCartaGiocoOrizzontale = "carta_da_gioco_orizzontale.jpg";
 	private boolean isScoperta;
+	private boolean isHorizontal;
 	
 	@Override
 	public void paint(Graphics g) 
@@ -23,16 +26,30 @@ public class ButtonCarta extends JButton
 		super.paint(g);
 		if (isScoperta)
 		{
-	        g.setColor(Color.WHITE);
-	        g.fillRect(0, 0, getWidth(), getHeight());
-	
+			g.setColor(Color.WHITE);
+			g.fillRect(0, 0, getWidth(), getHeight());
 	        g.setColor(Color.BLACK);
 	        g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
+	        
 	        if (valoreCarta.startsWith("♦") || valoreCarta.startsWith("♥"))
 	        	g.setColor(Color.RED);
-	
-	        g.setFont(new Font("Arial", Font.BOLD, 24));
-	        g.drawString(valoreCarta, 5, 25);
+	        Font font = new Font("Arial", Font.BOLD, 24);
+	        
+			if (isHorizontal)
+			{
+				AffineTransform affineTransform = new AffineTransform();
+				affineTransform.rotate(155);
+				//affineTransform.rotate(150);
+				//affineTransform.rotate(Math.toRadians(330), 5, 25);
+				Font rotatedFont = font.deriveFont(affineTransform);
+				g.setFont(rotatedFont);
+				g.drawString(valoreCarta, 27, 30);
+			}
+			else
+			{
+		        g.setFont(font);
+		        g.drawString(valoreCarta, 5, 25);
+			}
 		}
 	}
 	
@@ -56,6 +73,7 @@ public class ButtonCarta extends JButton
 	{
 		super();
 		this.valoreCarta = valoreCarta;
+		this.isHorizontal = isHorizontal;
 		try
 		{
 			/* Leggo l'immagine salvata nella directory "resources" dentro il progetto */
